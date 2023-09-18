4bb3724dbc9807ca34e9def68c7f19ae99908bae
paulocgr9 <126474247+paulocgr9@users.noreply.github.com>
2023-09-18 08:12:24 -0300

    Criado main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+tipo_de_entrada = input("Determine o tipo de entrada:\n(1) Console\n(2) Arquivo Texto\n")
+
+if tipo_de_entrada == "1":
+    pass
+elif tipo_de_entrada == "2":
+    pass
+else:
+    print("O número oferecido não é uma opção.")
