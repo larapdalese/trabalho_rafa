24a1c44f87534b602b59c8d740ef33bc1985d915
larapdalese <126460907+larapdalese@users.noreply.github.com>
2023-09-18 07:47:00 -0300

    Update aplicativo.py

--- aplicativo.py
+++ aplicativo.py
@@ -1,1 +1,4 @@
 #"A dupla deve elaborar um aplicativo que calcula o número de dias entre duas datas. O usuário pode fornecer duas formas distintas: 
+#- Através do comsole, utilizando o formato "18 de agosto de 2023 - 18 de setembro de 2023"
+
+#- Através de um arquivo texto, fornecendo seu nome. O conteúdo do arquivo deve respeitar a formatação de entrada estabelecida para o console. 
