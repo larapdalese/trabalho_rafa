abade13461f9c9acfb46690514de2a2b18ef4d13
paulocgr9 <126474247+paulocgr9@users.noreply.github.com>
2023-09-18 08:32:51 -0300

    Adicionado função para console

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import aplicativo as app
+
 tipo_de_entrada = input("Determine o tipo de entrada:\n(1) Console\n(2) Arquivo Texto\n")
 
 if tipo_de_entrada == "1":
-    pass
+    data = input("Insira as datas separadas por um hífen (-)")
+    data = data.split("-")
+    data_inicio = data[0]
+    data_fim = data[1]
+    print(app.numero_de_dias_entre_datas(data_fim, data_inicio))
 elif tipo_de_entrada == "2":
     pass
 else:
